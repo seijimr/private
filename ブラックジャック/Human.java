6174a069b7d05f82ff356ac39f1c946423626c4e
SeijiM <bugisfriend@gmail.com>
2016-10-04 17:15:46 +0900

    no message

---
+++ ブラックジャック/Human.java
@@ -0,0 +1,10 @@
+
+import java.util.*;
+
+
+abstract public class Human{
+    abstract public void open();
+   abstract public ArrayList setCard();
+   abstract public void checkSum();
+   List myCards=new ArrayList();
+}
